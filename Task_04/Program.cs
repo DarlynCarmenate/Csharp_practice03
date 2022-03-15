799bbd8e1128f782d92aea7320cba8bba80261c4
darlyncarmenate <darlyncarmenate@gmail.com>
2022-03-15 15:26:00 +0200

    Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+int number = new Random().Next(2, 30);
+Console.Write(number + "-> ");
+
+for(int i = 1; i <= number; i++){
+    int cube = i*i*i;
+    Console.Write(cube + " ");
+}
